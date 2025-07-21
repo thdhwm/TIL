41d41d9b008ffd9c1e1a8be46dbc81d469807618
John <my71564935@gmail.com>
2025-07-21 15:41:31 +0900

    basics_1

---
+++ Python_basics_1_2025_07_21/Basics_string_2983.py
@@ -0,0 +1,6 @@
+korean = '한글'
+english = 'english'
+number = 3
+
+print(f'{korean}과 {english}')
+print(f'{number * korean}')
---
+++ Python_basics_1_2025_07_21/hw_1_4.py
@@ -0,0 +1,11 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+
+apple = "사과는 영어로 apple "
+banana = "바나나는 영어로 banana"
+kiwi = "키위는 영어로 kiwi"
+
+print(apple)
+print(banana)
+print(kiwi)
