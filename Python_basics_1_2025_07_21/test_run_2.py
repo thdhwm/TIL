f86915cd419e9abec29a55f0789e373bab498b55
thdhwm <my71564935@gmail.com>
2025-07-27 16:04:08 +0900

    upload

--- Python_basics_1_2025_07_21/test_run_2.py
+++ Python_basics_1_2025_07_21/test_run_2.py
@@ -1,9 +1,9 @@
-T = int(input()) #태스트 케이스 수
+# T = int(input()) #태스트 케이스 수
 
-for t in range(1, T + 1):
-    day = int(input())
-    prices = list(map(int, input().split()))
-    print(max(prices))
+# for t in range(1, T + 1):
+#     day = int(input())
+#     prices = list(map(int, input().split()))
+#     print(max(prices))
 
 
 
@@ -18,11 +18,20 @@ for t in range(1, T + 1):
 
 # max_prices = [0]*day                           ## max_prices 라는 배열 만듬 [0, 0, 0]
 # max_price = prices[-1]                         ## max_price 를 prices 배열 마지막 원소로 지정 9
-# for i in range(day-1, -1, -1) :                range -> 0, -1, -1 
-# max_price = max(max_price, prices[i])          prices -> 
+# for i in range(day-1, -1, -1) :                # range -> 0, -1, -1 
+# max_price = max(max_price, prices[i])          # prices -> 
 # max_prices[i] = max_price
 
 # benefit = 0
 # for i in range(day) :
 # if max_prices[i] - prices[i] > 0 :
 # benefit += (max_prices[i] - prices[i])
+
+######################################################################################
+
+T = int(input())
+
+for t in range(1, T + 1):
+    N = int(input())
+
+    
