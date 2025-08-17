13e9d70af73c79c2781db3c53f645fd1faf4cdbe
thdhwm <my71564935@gmail.com>
2025-08-18 00:45:25 +0900

    upload

---
+++ Baek_jun_Solving/Samsung_A_Add_Brackets.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+formula = input().strip()
+max_outcome = -2**31
+# #######################################################
+def calc(n1, n2, operator):    # 연산, '+', '-', '*' 우선순위 동일
+    if operator == '+':
+        return n1 + n2
+    elif operator == '-':
+        return n1 - n2
+    elif operator == '*':
+        return n1 * n2
+
+def dfs(idx, current_value):    # idx - 갯수  # 현 위치(숫자)
+    global max_outcome
+
+    if idx == len(formula):
+        max_outcome = max(max_outcome, current_value)
+        return
+
+    next_value = calc(current_value, int(formula[idx + 1]), formula[idx])
+    dfs(idx + 2, next_value)
+
+    if idx + 4 <= len(formula):
+        bracket = calc(int(formula[idx + 1]), int(formula[idx + 3]), formula[idx + 2])
+        next_value = calc(current_value, bracket, formula[idx])
+        dfs(idx + 4, next_value)
+
+
+# ########################################################
+dfs(1, int(formula[0]))
+
+print(max_outcome)
