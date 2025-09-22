0a44d68bd286454606a527c28fbb1f0bc219b796
thdhwm <my71564935@gmail.com>
2025-09-22 23:57:05 +0900

    upload

--- Python/Baek_jun_Solving/Boj_13549_hide_n_seek_3.py
+++ Python/Baek_jun_Solving/Boj_13549_hide_n_seek_3.py
@@ -2,53 +2,53 @@
 # import sys
 # sys.stdin = open('input.txt')
 # # boj 13549
-#
+
 # def teleport(num):     # 2x 로 이동    ( 범위 안에 있는 )
 #     return 2 * num if 2 * num <= 100000 else -1
-#
-#
+
+
 # def left(num):     # x - 1 로 이동    ( 범위 안에 있는 )
 #     return num - 1 if num > 0 else -1
-#
-#
+
+
 # def right(num):    # x + 1 로 이동    ( 범위 안에 있는 )
 #     return num + 1 if num + 1 <= 100000 else -1
-#
-#
+
+
 # def hide_n_seek(start, end):     # bfs
 #     visited = [300000] * 100001
 #     visited[start] = 0
 #     funcs = [(teleport, 0), (right, 1), (left, 1)]
-#
+
 #     q = deque([(start, 0)])
-#
+
 #     if start == end:       # 이미 도착한 경우
 #         return 0
-#
+
 #     while q:
 #         now, moves = q.popleft()
-#
+
 #         for func, time in funcs:
 #             next = func(now)
 #             if next == -1:  # 범위 초과
 #                 continue
 #             new_time = moves + time
-#
+
 #             if visited[next] > new_time:
 #                 visited[next] = new_time
-#
+
 #                 # if next == end:
 #                 #     return new_time
-#
+
 #                 if time == 0:  # teleport 우선 처리
 #                     q.appendleft((next, new_time))
-#
+
 #                 else:  # 걸음 이동: 큐 뒤에 추가
 #                     q.append((next, new_time))
 #     return visited[end]
-#
+
 # N, M = map(int, input().split())
-#
+
 # print(hide_n_seek(N, M))
 
 # #######################################################################################
