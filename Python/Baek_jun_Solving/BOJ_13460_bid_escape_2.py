e0325929a5d20fe8655ec8a4fdfa31db8dc4dc8a
thdhwm <my71564935@gmail.com>
2025-09-29 16:57:30 +0900

    upload

--- Python/Baek_jun_Solving/BOJ_13460_bid_escape_2.py
+++ Python/Baek_jun_Solving/BOJ_13460_bid_escape_2.py
@@ -1,41 +1,89 @@
 from collections import deque
 import sys
 sys.stdin = open('input.txt')
+#
+#
+# def escape(table, red, blue, depth):
+#     q = deque([(table, red, blue, depth)])
+#
+#     while q:
+#         now_board, red_bid, blue_bid, now_depth = q.popleft()
+#         if now_depth == 11:
+#             return -1
+#
+#         nBoard = [row[:] for row in now_board]
+#         nRi, nRj = red_bid
+#         nBi, nBj = blue_bid
+#
+#         for k in range(4):
+#             newRi = nRi + di[k]
+#             newRj = nRj + dj[k]
+#             newBi = nBi + di[k]
+#             newBj = nBj + dj[k]
+#
+#             if newRi == ei and newRj == ej:
+#                 return now_depth
+#
+#             if nBoard[newRi][newRj] == '#' or nBoard[newBi][newBj] == 'O':
+#                 continue
+#
+#             if nBoard[newRi][newRj] == 'B' and nBoard[newBi][newBj] == '#':
+#                 continue
+#
+#             nBoard[nRi][nRj], nBoard[newRi][newRj] = nBoard[newRi][newRj], nBoard[nRi][nRj]
+#
+#             if nBoard[newBi][newBj] == '.':
+#                 nBoard[nBi][nBj], nBoard[newBi][newBj] = nBoard[newBi][newBj], nBoard[nBi][nBj]
+#
+#             q.append((nBoard, (newRi, newRj), (newBi, newBj), depth + 1))
+#
+#
+# N, M = map(int, input().split())
+# board = [list(input()) for _ in range(N)]
+# di = [-1, 0, 1, 0]
+# dj = [0, 1, 0, -1]
+#
+# for i in range(N):
+#     for j in range(M):
+#         if board[i][j] == 'R':
+#             ri, rj = i, j
+#
+#         elif board[i][j] == 'B':
+#             bi, bj = i, j
+#
+#         elif board[i][j] == 'O':
+#             ei, ej = i, j
+#
+# print(escape(board, (ri, rj), (bi, bj), 1))
+
+# ########################################################################################
 
 
-def escape(table, red, blue, depth):
-    q = deque([(table, red, blue, depth)])
+def tilt(depth, arr, red_bid, blue_bid):
+    now_board = arr[:]
 
-    while q:
-        now_board, red_bid, blue_bid, now_depth = q.popleft()
-        if now_depth == 11:
-            return -1
+    for k in range(4):
+        move(now_board, k, red_bid)
+    pass
 
-        nBoard = [row[:] for row in now_board]
-        nRi, nRj = red_bid
-        nBi, nBj = blue_bid
 
-        for k in range(4):
-            newRi = nRi + di[k]
-            newRj = nRj + dj[k]
-            newBi = nBi + di[k]
-            newBj = nBj + dj[k]
+def move(bd, direction, bid):
+    bid_i, bid_j = bid
 
-            if newRi == ei and newRj == ej:
-                return now_depth
+    for s in range(1, 10):   # vert, hor max 10
+        bid_i += di[direction]
+        bid_j += dj[direction]
 
-            if nBoard[newRi][newRj] == '#' or nBoard[newBi][newBj] == 'O':
-                continue
+        if bd[bid_i][bid_j] == '.':
+            continue
 
-            if nBoard[newRi][newRj] == 'B' and nBoard[newBi][newBj] == '#':
-                continue
+        elif bd[bid_i][bid_j] == '#':
+            bid_i -= di[direction]
+            bid_j -= dj[direction]
+            return bid_i, bid_j
 
-            nBoard[nRi][nRj], nBoard[newRi][newRj] = nBoard[newRi][newRj], nBoard[nRi][nRj]
-
-            if nBoard[newBi][newBj] == '.':
-                nBoard[nBi][nBj], nBoard[newBi][newBj] = nBoard[newBi][newBj], nBoard[nBi][nBj]
-
-            q.append((nBoard, (newRi, newRj), (newBi, newBj), depth + 1))
+        elif bd[bid_i][bid_j] == 'O':
+            pass #TODO 들어갔을 때 처리
 
 
 N, M = map(int, input().split())
@@ -54,6 +102,3 @@ for i in range(N):
         elif board[i][j] == 'O':
             ei, ej = i, j
 
-print(escape(board, (ri, rj), (bi, bj), 1))
-
-# ########################################################################################
--- Python/Baek_jun_Solving/BOJ_2251_water_bottles.py
+++ Python/Baek_jun_Solving/BOJ_2251_water_bottles.py
@@ -2,36 +2,37 @@ from collections import deque
 import sys
 sys.stdin = open('input.txt')
 
+
 def bottle(arr):
-  q = deque([arr])
-  limits = [A, B, C]
-  while q:
-      jugs = q.popleft()   # ex. 0, 0, 10
-      
-      for i in range(3):   # 0, 1, 2
-          if not jugs[i]:
-              continue
-          
-          for j in range(3):
-              next_jugs = jugs[:]
-              if i == j:
-                  continue    
+    q = deque([arr])
+    limits = [A, B, C]
+    while q:
+        jugs = q.popleft()   # ex. 0, 0, 10
+
+        for i in range(3):   # 0, 1, 2
+            if not jugs[i]:
+                continue
+
+            for j in range(3):
+                next_jugs = jugs[:]
+                if i == j:
+                    continue
+
+                if jugs[j] == limits[j]:
+                    continue
               
-              if jugs[j] == limits[j]:
-                  continue
+                waters = jugs[i] + jugs[j]
+                next_jugs[j] = min(waters, limits[j])
+                next_jugs[i] = waters - next_jugs[j]
               
-              waters = jugs[i] + jugs[j]
-              next_jugs[j] = min(waters, limits[j])
-              next_jugs[i] = waters - next_jugs[j]
-              
-              if tuple(next_jugs) in water_set:
-                  continue
-              
-              if next_jugs[0] == 0:
-                  result.add(next_jugs[2])
+                if tuple(next_jugs) in water_set:
+                    continue
+
+                if next_jugs[0] == 0:
+                    result.add(next_jugs[2])
                 
-              water_set.add(tuple(next_jugs))
-              q.append(next_jugs)
+                water_set.add(tuple(next_jugs))
+                q.append(next_jugs)
 
 
 A, B, C = map(int, input().split())
