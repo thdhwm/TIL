4fe8b4e9fa14ab64c91445ee557bb1f51937dc98
thdhwm <my71564935@gmail.com>
2025-11-19 17:55:28 +0900

    upload

--- Python/Baek_jun_Solving/BOJ_2011_Secret_code.py
+++ Python/Baek_jun_Solving/BOJ_2011_Secret_code.py
@@ -1,8 +1,26 @@
-dp = list(input().split())
+import sys
+sys.stdin = open('input.txt')
+
+data = list(map(int, input().split()))
+
+length = len(data)
+
+dp = [0] * (length + 1)
+dp[0] = 1
+dp[1] = 1
+
+for k in range(1, length):
+    i = k + 1
+    if data[k] > 0:
+        dp[i] += dp[i - 1]
+    if 10 <= data[k] + data[k - 1]*10 <= 26:
+        dp[i] += dp[i - 2]
+
+print(dp[length] % 1000000)
 
 
-
-
+# 1 00 00 0 0 0 0 0 0  이러면 어캄!
+#
 """
 25114
 
@@ -19,6 +37,3 @@ dp = list(input().split())
 [  [][], [][], [][], [][], [][]  ]
 
 """
-
-2   ->  1 가지
-2, 5   -> 1 가지 + 1 가지
